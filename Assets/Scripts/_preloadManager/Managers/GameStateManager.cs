20bb74654b3649a5200e7dcf579ecd2dcd73f43c
pardo312 <pardod479@gmail.com>
2020-08-27 18:14:59 -0500

    :rocket:Implementado Editor v0.1:rocket:

--- Assets/Scripts/_preloadManager/Managers/GameStateManager.cs
+++ Assets/Scripts/_preloadManager/Managers/GameStateManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameStateManager : MonoBehaviour
 {
     
-    public bool editing;
+    [HideInInspector]public bool editing;
     void Awake()
     {
         editing=true;
