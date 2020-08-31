f83ca887d6e04cf6f68cced2f5afd5438332f22a
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2020-08-31 16:05:34 -0500

    Mismo arreglo

--- Assets/Scripts/Game/Player/GunsLogic/Guns/FastRocket.cs
+++ Assets/Scripts/Game/Player/GunsLogic/Guns/FastRocket.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace BoomAway.Assets.Scripts.Game.Player.Guns;
-public class FastRocket : MonoBehaviour, IGun, IExplosive
+namespace BoomAway.Assets.Scripts.Game.Player.Guns
 {
-    public void explode(float radiousOfImpact, float explosionForce, LayerMask layerToHit)
+    public class FastRocket : MonoBehaviour, IGun, IExplosive
     {
 
+        private bool waitForRocket = false;
+
+        public void explode(float radiousOfImpact, float explosionForce, LayerMask layerToHit)
+        {
             Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, radiousOfImpact, layerToHit);
 
             foreach (Collider2D obj in objects)
@@ -16,23 +19,42 @@ public class FastRocket : MonoBehaviour, IGun, IExplosive
                 Vector2 direction = obj.transform.position - transform.position;
                 obj.GetComponent<Rigidbody2D>().AddForce(direction * explosionForce);
             }
-    }
+        }
 
-    public void shoot(float shootForce, BoxCollider2D bc, Rigidbody2D rb)
-    {
-        throw new System.NotImplementedException();
-    }
+        public void shoot(float shootForce, BoxCollider2D bc, Rigidbody2D rb)
+        {
+            if (!waitForRocket)
+            {
+                Vector3 tempPosition = transform.position;
+                transform.SetParent(null);
+                transform.position = tempPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+                bc.isTrigger = false;
+                rb.isKinematic = false;
+                rb.AddForce(transform.right * shootForce * -1);
 
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
+                Grid.gameStateManager.currentAmmo[1]--;
+                StartCoroutine(waitForFastRocket());
+            }
+        }
+
+        IEnumerator waitForFastRocket()
+        {
+            yield return new WaitForSeconds(1);
+            waitForRocket = true;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
     }
 
 }
