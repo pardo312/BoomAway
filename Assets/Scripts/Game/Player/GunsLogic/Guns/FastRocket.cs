f3a1c1d7593adecb72de6c06efd945902fa3bda4
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2020-08-31 09:09:54 -0500

    Antes de finalizar el fastrocket

---
+++ Assets/Scripts/Game/Player/GunsLogic/Guns/FastRocket.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BoomAway.Assets.Scripts.Game.Player.Guns;
+public class FastRocket : MonoBehaviour, IGun, IExplosive
+{
+    public void explode(float radiousOfImpact, float explosionForce, LayerMask layerToHit)
+    {
+        if ()
+        {
+
+            Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, radiousOfImpact, layerToHit);
+
+            foreach (Collider2D obj in objects)
+            {
+                Vector2 direction = obj.transform.position - transform.position;
+                obj.GetComponent<Rigidbody2D>().AddForce(direction * explosionForce);
+            }
+        }
+    }
+
+    public void shoot(float shootForce, BoxCollider2D bc, Rigidbody2D rb)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
