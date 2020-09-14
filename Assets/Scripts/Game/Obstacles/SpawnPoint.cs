a11c2554b4a9f17b422789ca12883ad5f1a5a4c1
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2020-09-14 17:12:04 -0500

    Listo el SpawnPoint

---
+++ Assets/Scripts/Game/Obstacles/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        bool editing = Grid.gameStateManager.editing;
+        if (!editing)
+        {
+            player.transform.position = gameObject.transform.position;
+            Destroy(gameObject);
+        }
+    }
+
+}
