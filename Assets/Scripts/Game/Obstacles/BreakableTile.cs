9f82e4b0d37c92f58444d81e76d6892c6fc5905c
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2020-09-14 22:40:07 -0500

    Arreglos Tag Destructible

--- Assets/Scripts/Game/Obstacles/BreakableTile.cs
+++ Assets/Scripts/Game/Obstacles/BreakableTile.cs
@@ -6,13 +6,26 @@ public class BreakableTile : MonoBehaviour
 {
 
     public bool explode;
+    public LayerMask layerToExplode;
 
     private void Update()
     {
         if (explode)
         {
-            GameObject.Destroy(gameObject);
+            Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, 2, layerToExplode);
+            
+            StartCoroutine(ExecuteAfterTime(0.5f, objects));
         }
+    }
+
+    IEnumerator ExecuteAfterTime(float time, Collider2D[] objects)
+    {
+        yield return new WaitForSeconds(time);
+        foreach (Collider2D obj in objects)
+        {
+            obj.GetComponent<BreakableTile>().explode = true;
+        }
+        Destroy(gameObject);
     }
 
 }
