3f8de6ce9be09b6bb09ce67e820143e524733fa8
pa-suarezm <pabloasuarezm@hotmail.com>
2020-10-02 00:27:54 -0500

    Primer acercamiento al tutorial

---
+++ Assets/Scripts/Tutorial/TutorialShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialShoot : MonoBehaviour
+{
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();        
+    }
+
+    private void OnEnable()
+    {
+        ActivateAnimAndChildren(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ActivateAnimAndChildren(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ActivateAnimAndChildren(false);
+        }
+    }
+
+    private void ActivateAnimAndChildren(bool activate)
+    {
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(activate);
+
+        anim.enabled = activate;
+    }
+}
