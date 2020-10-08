0523c63db6bfd5559720c9c16efba8efc505f40a
Jiufen <pardod479@gmail.com>
2020-10-08 11:36:59 -0500

    Fixed carga de niveles tutorial

--- Assets/Scripts/WorldBuilder/Maker/EndFlag.cs
+++ Assets/Scripts/WorldBuilder/Maker/EndFlag.cs
@@ -5,47 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class EndFlag : MonoBehaviour
 {
-    [SerializeField]private TimeOnLevel timeOnLevelScript;
+    [SerializeField] private TimeOnLevel timeOnLevelScript;
+    private bool alredyLoading = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (!alredyLoading)
         {
-            if (SceneManager.GetActiveScene().name.Equals("StoryLevel") || SceneManager.GetActiveScene().name.Equals("Tutorial"))
+            if (collision.CompareTag("Player"))
             {
-                timeOnLevelScript.uploadLevelCompletionTime();
-                switch (Grid.gameStateManager.currentLevel)
+                if (SceneManager.GetActiveScene().name.Equals("StoryLevel") || SceneManager.GetActiveScene().name.Equals("Tutorial"))
                 {
-                    default:
-                        Grid.gameStateManager.currentLevel = "LVL2";
-                        break;
-                    case "LVL2":
-                        Grid.gameStateManager.currentLevel = "LVL3";
-                        break;
-                    case "LVL3":
-                        Grid.gameStateManager.currentLevel = "LVL4";
-                        break;
-                    case "LVL4":
-                        Grid.gameStateManager.currentLevel = "LVL5";
-                        break;
-                    case "LVL5":
-                        Grid.gameStateManager.currentLevel = "LVL6";
-                        break;
-                    case "LVL6":
-                        Grid.gameStateManager.currentLevel = "LVL7";
-                        break;
-                    case "LVL7":
-                        Grid.gameStateManager.currentLevel = "LVL8";
-                        break;
-                    case "LVL8":
-                        Grid.gameStateManager.currentLevel = "LVL9";
-                        break;
-                    case "LVL9":
-                        SceneManager.LoadScene("TitleScreen");
-                        break;
-                    
+                    alredyLoading = true;
+                    timeOnLevelScript.uploadLevelCompletionTime();
+                    switch (Grid.gameStateManager.currentLevel)
+                    {
+                        default:
+                            Grid.gameStateManager.currentLevel = "LVL2";
+                            break;
+                        case "LVL2":
+                            Grid.gameStateManager.currentLevel = "LVL3";
+                            break;
+                        case "LVL3":
+                            Grid.gameStateManager.currentLevel = "LVL4";
+                            break;
+                        case "LVL4":
+                            Grid.gameStateManager.currentLevel = "LVL5";
+                            break;
+                        case "LVL5":
+                            Grid.gameStateManager.currentLevel = "LVL6";
+                            break;
+                        case "LVL6":
+                            Grid.gameStateManager.currentLevel = "LVL7";
+                            break;
+                        case "LVL7":
+                            Grid.gameStateManager.currentLevel = "LVL8";
+                            break;
+                        case "LVL8":
+                            Grid.gameStateManager.currentLevel = "LVL9";
+                            break;
+                        case "LVL9":
+                            SceneManager.LoadScene("TitleScreen");
+                            break;
+
+                    }
+                    SceneManager.LoadScene("StoryLevel");
                 }
-                SceneManager.LoadScene("StoryLevel");
             }
         }
+
     }
 }
