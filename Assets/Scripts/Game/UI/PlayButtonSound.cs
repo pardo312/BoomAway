7531baa931b6a16c7b8500ea43b440c792eb849b
Jiufen <da.pardo@uniandes.edu.co>
2020-10-21 17:39:32 -0300

    Añadido sonido de botones

---
+++ Assets/Scripts/Game/UI/PlayButtonSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonSound : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void playSound()
+    {
+        Grid.audioManager.Play("ButtonFX");
+    }
+}
